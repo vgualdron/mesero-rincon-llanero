103a8828c4c6b192a87bd9760a5a841487310c82
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2022-11-02 19:10:15 -0500

    sad

--- src/containers/DefaultHeaderDropdownAccnt.vue
+++ src/containers/DefaultHeaderDropdownAccnt.vue
@@ -1,6 +1,7 @@
 <template>
   <AppHeaderDropdown right no-caret>
     <template slot="header">
+      <h5 style="line-height: 2.2;display: inline;">{{ username }}</h5>
       <img
         src="static/img/avatar-profile.jpg"
         class="img-avatar"
@@ -8,33 +9,6 @@
     </template>\
     <template slot="dropdown">
       <b-dropdown-header tag="div" class="text-center"><strong>Cuenta</strong></b-dropdown-header>
-      <!--<b-dropdown-item><i class="fa fa-bell-o" /> Updates
-        <b-badge variant="info">{{ itemsCount }}</b-badge>
-      </b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-envelope-o" /> Messages
-        <b-badge variant="success">{{ itemsCount }}</b-badge>
-      </b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-tasks" /> Tasks
-        <b-badge variant="danger">{{ itemsCount }}</b-badge>
-      </b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-comments" /> Comments
-        <b-badge variant="warning">{{ itemsCount }}</b-badge>
-      </b-dropdown-item>
-      <b-dropdown-header
-        tag="div"
-        class="text-center">
-        <strong>Settings</strong>
-      </b-dropdown-header>
-      <b-dropdown-item><i class="fa fa-user" /> Profile</b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-wrench" /> Settings</b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-usd" /> Payments
-        <b-badge variant="secondary">{{ itemsCount }}</b-badge>
-      </b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-file" /> Projects
-        <b-badge variant="primary">{{ itemsCount }}</b-badge>
-      </b-dropdown-item>
-      <b-dropdown-divider />
-      <b-dropdown-item><i class="fa fa-shield" /> Lock Account</b-dropdown-item>-->
       <b-dropdown-item @click="cerrarSesion()"><i class="fa fa-lock" /> Cerrar Pestaña</b-dropdown-item>
     </template>
   </AppHeaderDropdown>
@@ -52,6 +26,11 @@ export default {
       localStorage.removeItem('token')
       location.href = '/'
       // window.close()
+    }
+  },
+  computed: {
+    username () {
+      return localStorage.getItem('username')
     }
   },
   data: () => {
